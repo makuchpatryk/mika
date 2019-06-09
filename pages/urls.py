ee50aecb3d474c8c4046ab0daeee3fea5b06ff39
patryk makuch <makuchpatryk@gmail.com>
2019-06-09 22:36:16 +0100

    second commit

--- pages/urls.py
+++ pages/urls.py
@@ -6,10 +6,10 @@ from . import views
 urlpatterns = [
     url(r'^$', views.IndexPageView.as_view(), name='index'),
     url(r'^about/$', views.AboutPageView.as_view(), name='about'),
-    url(r'^blog/$', views.AboutPageView.as_view(), name='blog'),
-    url(r'^blog-single/$', views.AboutPageView.as_view(), name='blog_single'),
-    url(r'^contact/$', views.AboutPageView.as_view(), name='contact'),
-    url(r'^portfolio/$', views.AboutPageView.as_view(), name='portfolio'),
-    url(r'^portfolio-single/$', views.AboutPageView.as_view(),name='portfolio_single'),
-    url(r'^services/$', views.AboutPageView.as_view(), name='services')
+    url(r'^blog/$', views.BlogPageView.as_view(), name='blog'),
+    url(r'^blog-single/$', views.BlogSinglePageView.as_view(), name='blog_single'),
+    url(r'^contact/$', views.ContactPageView.as_view(), name='contact'),
+    url(r'^portfolio/$', views.PortfolioPageView.as_view(), name='portfolio'),
+    url(r'^portfolio-single/$', views.PortfolioSinglePageView.as_view(),name='portfolio_single'),
+    url(r'^services/$', views.ServicesPageView.as_view(), name='services')
 ]
