2dd2036b4cbadc8545bbc1b8170f272c628ef37d
patryk makuch <makuchpatryk@gmail.com>
2019-08-29 20:48:53 +0100

    add slider

--- static/js/main.js
+++ static/js/main.js
@@ -16,3 +16,23 @@ $(document).ready(function()
     audio.pause();
   });
 });
+
+$(document).ready(function()
+{
+    x = 1;
+    $('#slider ul li:nth-child(' + x + ')').show();
+    setInterval(function () {
+        nextSlide();
+    }, 5000);
+
+    function nextSlide() {
+      $('#slider ul li:nth-child(' + x + ')').hide();
+      if (x > 5)
+      {
+        x = 0;
+      }
+        x++;
+        $('#slider ul li:nth-child(' + x + ')').fadeIn('fast');
+    };
+
+});
