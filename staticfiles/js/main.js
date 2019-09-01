ced4b11345a229e5e8aa8990e4d6a7df890671c0
Patryk <makuchpatryk@gmail.com>
2019-09-01 02:12:02 +0200

    minor

---
+++ staticfiles/js/main.js
@@ -0,0 +1,64 @@
+$(document).ready(function()
+{
+  var audio = new Audio();
+  audio.src = data['source'];
+  audio.id = 'audio';
+
+  audio.volume = 0.05;
+  audio.autoplay = true;
+  audio.loop = true;
+
+  $( "#musicplay" ).click(function() {
+    audio.play();
+  });
+  $( "#musicpause" ).click(function() {
+    audio.pause();
+  });
+
+	playPromise = audio.play();
+	if (playPromise !== undefined) {
+	  playPromise.then(function() {
+			console.log('started');
+	  }).catch(function(error) {
+			console.log('error');
+      $( "#musicplay" ).click()
+	  });
+	}
+
+  audio.addEventListener('ended', function() {
+    this.currentTime = 0;
+    this.play();
+  }, false);
+
+	$(window).load(function() {
+		// Animate loader off screen
+		$(".se-pre-con").fadeOut("slow");
+	});
+    x = 1;
+    $('#slider ul li:nth-child(' + x + ')').show();
+    setInterval(function () {
+        nextSlide();
+    }, 5000);
+
+    function nextSlide() {
+      $('#slider ul li:nth-child(' + x + ')').fadeOut(1000);
+      if (x > 5)
+      {
+        x = 0;
+      }
+        x++;
+        $('#slider ul li:nth-child(' + x + ')').fadeIn(1200);
+    };
+    var navbar = document.getElementById("navbar");
+    var sticky = navbar.offsetTop;
+    myFunction()
+    window.onscroll = function() {myFunction()};
+
+    function myFunction() {
+      if (window.pageYOffset > sticky) {
+        navbar.classList.add("sticky")
+      } else {
+        navbar.classList.remove("sticky");
+      }
+    }
+});
