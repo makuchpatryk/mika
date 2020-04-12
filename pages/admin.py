26639cb7edafba5bc52859b3da523e483ea4a78f
patryk makuch <makuchpatryk@gmail.com>
2020-04-12 13:28:51 +0100

    add mika modal album

--- pages/admin.py
+++ pages/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Category, Post
+from .models import Category, Post, Album, Song
 
 admin.site.register(Category)
 admin.site.register(Post)
+admin.site.register(Album)
+admin.site.register(Song)
--- pages/models.py
+++ pages/models.py
@@ -1,12 +1,46 @@
 from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import User
+from django.urls import reverse
+
 # Create your models here.
 
 
 class Category(models.Model):
     title = models.CharField(max_length=255, verbose_name="Title")
 
+    def __str__(self):
+        return self.title
 
 class Post(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     title = models.CharField(max_length=255, verbose_name="Title")
+    image = models.ImageField(upload_to ='uploads/', blank=True)
+    ctime = models.DateTimeField(auto_now_add=True, null=True, blank=True)
     text = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+
+class Album(models.Model):
+    album_name = models.CharField(max_length=250)
+    album_picture = models.ImageField(upload_to ='uploads/', blank=True)
+    description = models.CharField(max_length=250)
+    ctime = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    author = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.author.get_username() + ' - ' + self.album_name
+
+
+class Song(models.Model):
+    album = models.ForeignKey(Album, on_delete=models.CASCADE, null=True)
+    cover = models.ImageField(upload_to ='uploads/', blank=True)
+    ctime = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+
+    song_name= models.CharField(max_length=1000)
+    description = models.CharField(max_length=250)
+
+    def __str__(self):
+        return  self.song_name
