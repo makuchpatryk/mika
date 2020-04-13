8b5bc98aa6de4bd04f748bf3d75e4c3259b62349
patryk makuch <makuchpatryk@gmail.com>
2020-04-13 13:31:26 +0100

    fix static setting

--- mika/base.py
+++ mika/base.py
@@ -124,7 +124,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-STATIC_ROOT = '/storage/home/django_learn/storage/static/'
 
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
--- mika/development.py
+++ mika/development.py
@@ -2,3 +2,5 @@ import os
 from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+
+STATIC_ROOT = '/storage/home/django_learn/storage/static/'
