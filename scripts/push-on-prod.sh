20511fc45cccec320ca959e7cdd29ecb7ef181d4
patryk makuch <makuchpatryk@gmail.com>
2020-04-20 20:55:55 +0100

    add like and refactor code

--- scripts/push-on-prod.sh
+++ scripts/push-on-prod.sh
@@ -2,4 +2,5 @@
 ssh makuchpatryk.xyz << "EOF"
 cd /home/patryk/project/mika
 git pull origin master
+sudo service uwsgi restart
 EOF
---
+++ scripts/version_up.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+increment_version ()
+{
+  declare -a part=( ${1//\./ } )
+  declare    new
+  declare -i carry=1
+
+  for (( CNTR=${#part[@]}-1; CNTR>=0; CNTR-=1 )); do
+    len=${#part[CNTR]}
+    new=$((part[CNTR]+carry))
+    [ ${#new} -gt $len ] && carry=1 || carry=0
+    [ $CNTR -gt 0 ] && part[CNTR]=${new: -len} || part[CNTR]=${new}
+  done
+  new="${part[*]}"
+  echo -e "${new// /.}" > version
+  git add version
+  git commit -m "version ${new}"
+  echo $new
+}
+
+vers="$(cat version)"
+increment_version $vers
