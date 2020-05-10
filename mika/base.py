f3f5983edfc71322c7aac96b85689d323fa76f47
patryk makuch <makuchpatryk@gmail.com>
2020-05-10 20:25:49 +0100

    add comment post on blog

--- mika/base.py
+++ mika/base.py
@@ -36,10 +36,10 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.sitemaps',
     'django.contrib.staticfiles',
     'pages',
     'rest_framework',
-    'django.contrib.sitemaps',
 ]
 
 MIDDLEWARE = [
